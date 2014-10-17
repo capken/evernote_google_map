2e44dc42f0130e11283599c41b2a2c9bfd2fbab3
Allen Zheng <capken@gmail.com>
2014-10-17 02:44:16 -0400

    setup unicorn + nginx succesfully

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /home/allen/codes/evernote_google_map/
-unicorn -c ./deploy/unicorn.rb -E production -D
+unicorn -c deploy/unicorn.rb -D
 sudo cp deploy/nginx.conf /etc/nginx/nginx.conf
 sudo /etc/init.d/nginx start
